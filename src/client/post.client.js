1cf96aa9bb31e90b36e67917fa72f26938fcb98d
edgar <harove@gmail.com>
2019-12-27 00:38:04 -0300

    post.client implemented

--- src/client/post.client.js
+++ src/client/post.client.js
@@ -2,7 +2,7 @@ import { API_HOST } from './config';
 
 export const findAll = () => {
     const token = localStorage.getItem('token');
-    return fetch(`${API_HOST}/api/user`, {
+    return fetch(`${API_HOST}/api/post`, {
         method: 'GET', // or 'PUT'
         headers: {
             'Content-Type': 'application/json',
@@ -13,7 +13,7 @@ export const findAll = () => {
 
 export const findById = (id) => {
     const token = localStorage.getItem('token');
-    return fetch(`${API_HOST}/api/user/${id}`, {
+    return fetch(`${API_HOST}/api/post/${id}`, {
         method: 'GET', // or 'PUT'
         headers: {
             'Content-Type': 'application/json',
@@ -27,11 +27,11 @@ email,
 password
 name
 */
-export const save = (user) => {
+export const save = (recipe) => {
     const token = localStorage.getItem('token');
-    return fetch(`${API_HOST}/api/user`, {
+    return fetch(`${API_HOST}/api/post`, {
         method: 'POST', // or 'PUT'
-        body: JSON.stringify(user),
+        body: JSON.stringify(recipe),
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`
